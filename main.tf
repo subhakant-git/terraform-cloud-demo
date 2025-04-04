729955c95711f8d35bdd7f094d6960e5c495b492
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2025-04-04 08:06:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,64 +1,26 @@
 provider "aws" {
-  region = "us-east-2"
+region = "us-east-1"
 
 }
-
-
-provider "aws" {
-  alias = "region01"
-  region = "us-west-1"
-
-}
-
-
-provider "aws" {
-  alias = "region02"
-  region = "us-west-1"
-
-}
-
-
-
-
 
 data "aws_ami" "myami"{
 
-provider = aws.region02
-
-most_recent = true
-
 owners = ["amazon"]
+most_recent = true
 
 filter {
     name   = "name"
     values = ["amzn2-ami-hvm*"]
   }
 
+
 }
 
 resource "aws_instance" "myec2" {
-  provider = aws.region02
+  ami    = data.aws_ami.myami.id 
+  instance_type = "t2.micro"
 
-  ami           = data.aws_ami.myami.id
-  instance_type = "t2.large"
-
-
-
-}
-
-
-resource "aws_vpc" "sl-vpc" {
- provider = aws.region02
-  cidr_block       = "0.0.0.0/16"
-   tags = {
-    Name = "sl-vpc"
+  tags = {
+    Name = "Instance-1"
   }
 }
-
-
-
-
-
-
-
-
